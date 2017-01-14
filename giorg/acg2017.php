2edd410dab7e463372bdc2efdac2e9d97767d3f9
Kevin Klein <kevin.klein@gmail.com>
2017-01-13 18:26:54 -0800

    add base for gi.org

---
+++ giorg/acg2017.php
@@ -0,0 +1,276 @@
+<!doctype html>
+<html class="no-js" lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <title>ACG 2017</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?php include('partials/head.php') ?> 
+</head>
+<body class="fixed-top">
+    
+<?php include('partials/header.php') ?>  
+
+<div> 
+    
+    <nav class="bg-secondary" style="box-shadow: 0 4px 0 rgba(0,0,0,.15);position: relative; z-index: 2;">
+        <div class="container text-center text-inverse">
+            <a href="#" class="btn btn-link btn-lg p-a p-t">Registration</a>
+            <a href="#" class="btn btn-link btn-lg p-a p-t">Abstracts</a>
+            <a href="#" class="btn btn-link btn-lg p-a p-t">Programs</a>
+            <a href="#" class="btn btn-link btn-lg p-a p-t">Exhibitors & Sponsors</a>
+            <a href="#" class="btn btn-link btn-lg p-a p-t">About</a>
+            <a href="#" class="btn btn-link btn-lg p-a p-t p-r-0">Media</a>
+        </div>
+    </nav>
+    
+    <div class="bg-secondary-dark text-inverse" style="position: relative; z-index: 1;">
+        <div class="container">
+            <div class="row-flex">
+                <div class="col-md-8 col-xs-12 text-webfont-one">
+                    <div class="p-y-lg p-x-md p-lg-r-xl position-relative">
+                        <h1 class="text-black">ACG 2017</h1>
+                        <p class="display-1 text-muted p-lg-r-xl m-b">Oct. 15-20. The premier GI clinical meeting and postgraduate course returns to Las Vegas, Nevada.</p>
+                        <a href="#" class="btn btn-outline btn-outline-inverse">Register Now <svg class="icon icon-chevron-with-circle-right"><use xlink:href="#icon-chevron-with-circle-right"></use></svg></a>
+                        <img src="img/venetian-ribbon.png" alt="venetian-ribbon" class="hidden-sm-down" width="190" height="190" style="position:absolute;top:0;right:-15px;">
+                    </div>
+                </div>
+                <div class="col-md-4  col-xs-12" style="background-image: url(img/venetian.jpg);background-size: cover">
+                   <img src="img/venetian.jpg" class="hidden-md-up">
+                </div>
+            </div>
+        </div><!--/container--> 
+    </div>
+    
+    <div class="bg-gray-light">
+        <div class="container">
+            <div class="row-flex">
+                <div class="col-md-8">
+                    <div class="p-a-md p-lg-r-xl">
+                        <h2 class="display-2">Join the American College of Gastroenterology and your colleagues in Las Vegas</h2>
+                        <p class="lead m-b-0">ACG 2017 meeting will be held in Las Vegas, Nevada at The Venetian. The faculty includes internationally known experts and rising stars in the fields of gastroenterology and hepatology, delivering the latest clinical updates on common and not-so-common GI disorders, as well as what is on the
+horizon that may impact your practice. <!-- <a href="#" class="text-underline">View the Preliminary Program</a --></p>
+                    </div>
+                </div>
+                <div class="col-md-4 bg-gray-lighter">
+                    <div class="p-a-md">
+                        <h3 class="text-uppercase text-normal text-muted m-b-md m-t"><b>I Want To…</b></h3>
+                        <div class="row">
+                            <div class="col-md-6">
+                                <ul class="text-sm text-emphasis list-unstyled">
+                                    <li><a href="#">Register for ACG 2017</a></li>
+                                    <li><a href="#">View the Preliminary Program</a></li>
+                                    <li><a href="#">Submit an Abstract for consideration</a></li>
+                                    <li><a href="#">Learn about travel and hotel discounts</a></li>
+                                </ul>
+                            </div>
+                            <div class="col-md-6">
+                                <ul class="text-sm text-emphasis list-unstyled">
+                                    <li><a href="#">Exhibit at ACG 2017 in Las Vegas</a></li>
+                                    <li><a href="#">Print CME and Download Slides from ACG 2016</a></li>
+                                    <li><a href="#">Learn about what to do and see in Vegas</a></li>
+                                </ul>   
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    <div class="bg-tertiary">
+        <div class="container p-y-lg text-inverse">
+            <div class="row-flex middle-xs">
+                <div class="col-md-8 col-xs-12">
+                    <h2 class="m-b-0 text-center text-webfont-two display-2">Registration now open for ACG 2017</h2> 
+                </div>
+                <div class="col-md-4 col-xs-12">
+                    <div class="p-lg-x-lg">
+                        <a href="#" class="btn btn-opaque-dark btn-lg btn-block">Register Now <svg class="icon icon-chevron-with-circle-right"><use xlink:href="#icon-chevron-with-circle-right"></use></svg></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    <div class="bg-gray-lighter">
+        <div class="container p-a-lg">
+            
+            <div class="bg-white p-a-md raised">
+                <div class="text-center"><svg class="icon icon-calendar display-2 text-muted"><use xlink:href="#icon-calendar"></use></svg></div>
+                <h3 class="text-center text-md text-uppercase text-dark m-t-sm m-b-md">Schedule at a Glance</h3>
+                <div class="row m-b-md">
+                    <div class="col-md-2">
+                        <a class="p-a-sm border-all text-center display-block bg-tertiary text-no-underline text-inverse" href="#">
+                            <b class="text-sm text-uppercase text-mute">Friday, Oct.</b>
+                            <div class="h2"><b>18</b></div>
+                        </a>  
+                    </div>
+                    <div class="col-md-2">
+                        <a class="p-a-sm border-all text-center display-block text-no-underline hover-bg-opaque-dark" href="#">
+                            <b class="text-sm text-uppercase text-mute">Saturday, Oct.</b>
+                            <div class="h2"><b>19</b></div>
+                        </a>  
+                    </div>
+                    <div class="col-md-2">
+                        <a class="p-a-sm border-all text-center display-block text-no-underline hover-bg-opaque-dark" href="#">
+                            <b class="text-sm text-uppercase text-mute">Sunday, Oct.</b>
+                            <div class="h2"><b>20</b></div>
+                        </a>  
+                    </div>
+                    <div class="col-md-2">
+                        <a class="p-a-sm border-all text-center display-block text-no-underline hover-bg-opaque-dark" href="#">
+                            <b class="text-sm text-uppercase text-mute">Monday, Oct.</b>
+                            <div class="h2"><b>21</b></div>
+                        </a>  
+                    </div>
+                    <div class="col-md-2">
+                        <a class="p-a-sm border-all text-center display-block text-no-underline hover-bg-opaque-dark" href="#">
+                            <b class="text-sm text-uppercase text-mute">Tuesday, Oct.</b>
+                            <div class="h2"><b>22</b></div>
+                        </a>  
+                    </div>
+                    <div class="col-md-2">
+                        <a class="p-a-sm border-all text-center display-block text-no-underline hover-bg-opaque-dark" href="#">
+                            <b class="text-sm text-uppercase text-mute">Wednesday, Oct.</b>
+                            <div class="h2"><b>23</b></div>
+                        </a>  
+                    </div>
+                </div>
+                
+                <div class="row">
+                    <div class="col-md-3">
+                        <h4 class="h6 m-b-xs">Registration</h4>
+                        <p class="text-sm text-muted m-b-xs">Level 1 Registration</p>
+                        <p class="text-xs text-mute">6:00 am – 5:00 pm</p>
+                        <hr>
+                        <h4 class="h6 m-b-xs">Continental Breakfast</h4>
+                        <p class="text-sm text-muted m-b-xs">Outside course rooms</p>
+                        <p class="text-xs text-mute">6:30 am – 7:30 am</p>
+                    </div>
+                    <div class="col-md-3">
+                        <h4 class="h6 m-b-xs">GI Pathology and Imaging Course</h4>
+                        <p class="text-sm text-muted m-b-xs">Room TBD</p>
+                        <p class="text-xs text-mute">7:00 am – 12:30 pm</p>
+                        <hr>
+                        <h4 class="h6 m-b-xs">Practice Management Course</h4>
+                        <p class="text-sm text-muted m-b-xs">Room TBD</p>
+                        <p class="text-xs text-mute">7:30 am – 4:30 pm</p>
+                    </div>
+                    <div class="col-md-3">
+                        <h4 class="h6 m-b-xs">ASGE-Sponsored Course</h4>
+                        <p class="text-sm text-muted m-b-xs">Room TBD</p>
+                        <p class="text-xs text-mute">7:30 am – 3:30 pm</p>
+                        <hr>
+                        <h4 class="h6 m-b-xs">What’s New in GI Pharmacology Course</h4>
+                        <p class="text-sm text-muted m-b-xs">Room TBD</p>
+                        <p class="text-xs text-mute">1:45 pm – 5:00 pm</p>
+                    </div>
+                    <div class="col-md-3">
+                        <h4 class="h6 m-b-xs">Navigating, Networking, and Negotiating Your First Job Workshop*</h4>
+                        <p class="text-sm text-muted m-b-xs">Rooms 2301-2302</p>
+                        <p class="text-xs text-mute">5:00 pm – 9:00 pm</p>
+                    </div>
+                </div>
+                
+            </div>
+            
+            <div class="row m-t-lg">
+                <div class="col-md-4 p-b p-lg-b-0">
+                    <div class="card match-height">
+                        <div class="card-header bg-secondary-dark text-inverse">
+                            <h3 class="h4 m-a-0"><a href="#" class="text-black"><!-- <svg class="icon icon-users pull-right"><use xlink:href="#icon-users"></use></svg> --> Hands-on Workshops</a></h3>
+                        </div>
+                        <div class="card-block">
+                        
+                            <ul class="list-unstyled">
+                                <li class="list-item list-icon">
+                                    <svg class="icon icon-calendar"><use xlink:href="#icon-calendar"></use></svg>
+                                    <a href="#" class="link-block">
+                                        <h4 class="h6 text-emphasis m-b-xs">Upper EMR</h4>
+                                        <div class="text-sm text-muted">Faculty Leader: Kenneth K. Wang, MD, FACG</div>
+                                        <div class="text-sm text-mute">Monday, Oct. 16, 3:45 pm - 6:45 pm</div>
+                                    </a>
+                                </li>
+                                <li class="list-item list-icon">
+                                    <svg class="icon icon-calendar"><use xlink:href="#icon-calendar"></use></svg>
+                                    <a href="#" class="link-block">
+                                        <h4 class="h6 text-emphasis m-b-xs">EMR/Polypectomy</h4>
+                                        <div class="text-sm text-muted">Faculty Leader:  Andrew Y. Wang, MD, FACG</div>
+                                        <div class="text-sm text-mute">Tuesday, Oct. 17, 10:15 am - 1:15 pm</div>
+                                    </a>
+                                </li>
+                                <li class="list-item list-icon">
+                                    <svg class="icon icon-calendar"><use xlink:href="#icon-calendar"></use></svg>
+                                    <a href="#" class="link-block">
+                                        <h4 class="h6 text-emphasis m-b-xs">Esophageal & Anorectal Motility Disorders</h4>
+                                        <div class="text-sm text-muted">Faculty Leader: John E. Pandol no, MD, MSCI, FACG</div>
+                                        <div class="text-sm text-mute">Wednesday, Oct. 18, 1:30 pm - 4:30 pm</div>
+                                    </a>
+                                </li>
+                                <li class="list-item list-icon">
+                                    <svg class="icon icon-calendar"><use xlink:href="#icon-calendar"></use></svg>
+                                    <a href="#" class="link-block">
+                                        <h4 class="h6 text-emphasis m-b-xs">Hemorrhoid Therapy</h4>
+                                        <div class="text-sm text-muted">Faculty Leader: Waqar A. Qureshi, MD, FACG</div>
+                                        <div class="text-sm text-mute">Wednesday, Oct. 18, 1:30 pm - 4:30 pm</div>
+                                    </a>
+                                </li>
+                            </ul>
+                            <div class="text-right">
+                                <a href="#" class="btn btn-primary">View All Workshops <svg class="icon icon-chevron-with-circle-right"><use xlink:href="#icon-chevron-with-circle-right"></use></svg></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card match-height">
+                        <a href="#"><img class="card-img" src="img/stock2.jpg"></a>
+                        <div class="card-block">
+                            <h3><a href="#" class="text-black">Fellowships in Training</a></h3>
+                            <p class="text-muted text-normal">Special trainee-focused offerings at ACG 2016 include the Trainees Luncheon, Career Opportunities for Women in GI Luncheon, and the Navigating, Networking, and Ne- gotiating Your First Job Workshop.</p>
+                            <div class="text-right">
+                                <a href="#" class="btn btn-primary">Fellowships in Training Events <svg class="icon icon-chevron-with-circle-right"><use xlink:href="#icon-chevron-with-circle-right"></use></svg></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card match-height">
+                        <img class="card-img" src="img/vegas.jpg">
+                        <div class="card-block">
+                            <h3>Las Vegas, Nevada</h3>
+                            <p class="text-muted text-normal">We're excited to return to Las Vegas in 2017. Here's your guide to the city:</p>
+                            <ul class="list-unstyled">
+                                <li class="list-icon">
+                                    <svg class="icon"><use xlink:href="#icon-location_city"></use></svg> 
+                                    <a href="#"><b>Hotel and lodging</b></a>
+                                </li>
+                                <li class="list-icon">
+                                    <svg class="icon"><use xlink:href="#icon-local_airport"></use></svg> 
+                                    <a href="#"><b>Flight info and airlines</b></a>
+                                </li>
+                                <li class="list-icon">
+                                    <svg class="icon"><use xlink:href="#icon-restaurant_menu"></use></svg> 
+                                    <a href="#"><b>Where to eat in Vegas</b></a>
+                                </li>
+                                <li class="list-icon">
+                                    <svg class="icon"><use xlink:href="#icon-local_bar"></use></svg> 
+                                    <a href="#"><b>Vegas nightlife</b></a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+    </div>
+ 
+</div><!-- /main div -->    
+
+<?php include('partials/footer.php') ?>   
+<?php include('partials/foot.php') ?>     
+
+</body>
+</html>
